af277e0ea12dbe869419a8eeb4c80a8b14cb1c02
Bartosz Jagłowski <lightheaded97@gmail.com>
2023-11-05 15:04:04 +0100

    cleanup urls.py

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # add your app urls here
-    # path('myapp/', include('myapp.urls')),
 ]
