845869519b702df02cd2fb9c1b5993b056ba3723
Illia Nebeskyi <illianebeskyi@gmail.com>
2024-01-19 01:03:26 +0100

    Add auction related views

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -5,6 +5,8 @@ from rest_framework_simplejwt import views as jwt_views
 
 from app.views.user_creation import RegistrationView, LoginView, LogoutView, ChangePasswordView
 from app.views.base import ServerStatView, UserView, AuctionView, ChatView, MessageView  # , BuyView
+from app.views.auction import AddAuctionView, EditAuctionView, BuyView
+from app.views.chat import ListChatsView, ListChatMessagesView, SendMessageView
 
 
 router = routers.DefaultRouter()
@@ -22,6 +24,11 @@ urlpatterns = [
     path('api/logout', LogoutView.as_view(), name='logout'),
     path('api/change-password', ChangePasswordView.as_view(), name='change-password'),
     path('api/token-refresh', jwt_views.TokenRefreshView.as_view(), name='token-refresh'),
-    # path('api/buy', BuyView.as_view(), name='buy'),
+    path('api/auctions/buy/<int:auction_id>', BuyView.as_view(), name='buy'),
+    path('api/auctions/add-auction', AddAuctionView.as_view(), name='add-auction'),
+    path('api/auctions/edit-auction/<int:auction_id>/', EditAuctionView.as_view(), name='edit-auction'),
+    # path('api/chats/', ListChatsView.as_view(), name='list-chats'),
+    # path('api/chats/auctions/<int:auction_id>/', ListChatMessagesView.as_view(), name='list-chat-messages'),
+    # path('api/chats/auctions/<int:auction_id>/send-message/', SendMessageView.as_view(), name='send-message'),
     path('api/', include(router.urls)),
 ]
