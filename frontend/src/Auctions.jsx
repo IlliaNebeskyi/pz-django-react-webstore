ca7e17d064d69e79016cc1995d092a7cabb02f0d
Illia Nebeskyi <illianebeskyi@gmail.com>
2024-01-21 04:53:17 +0100

    Add auctions sorting

--- frontend/src/Auctions.jsx
+++ frontend/src/Auctions.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import Chat from "./Chat";
 import AddAuction from "./AddAuction";
@@ -6,15 +6,18 @@ import EditAuction from "./EditAuction";
 import Chats from "./Chats";
 
 function Auctions({
-                      username,
-                      isLoggedIn,
-                  }) {
+    username,
+    isLoggedIn,
+}) {
     const [auctions, setAuctions] = useState([]);
     const [isChatActive, setIsChatActive] = useState(false);
     const [isChatsActive, setIsChatsActive] = useState(false);
     const [isAddAuctionActive, setIsAddAuctionActive] = useState(false);
     const [isEditAuctionActive, setIsEditAuctionActive] = useState(false);
     const [activeAuction, setActiveAuction] = useState(false);
+    const [sortField, setSortField] = useState('price');
+    const [sortDirection, setSortDirection] = useState('asc');
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -93,28 +96,63 @@ function Auctions({
             });
     }
 
+    const sortAuctions = () => {
+        return [...auctions].sort((a, b) => {
+            let aValue = a[sortField];
+            let bValue = b[sortField];
+
+            // Check if the sort field is 'price' and convert to float for comparison
+            if (sortField === 'price') {
+                aValue = parseFloat(aValue.replace('$', ''));
+                bValue = parseFloat(bValue.replace('$', ''));
+            }
+
+            if (aValue < bValue) {
+                return sortDirection === 'asc' ? -1 : 1;
+            }
+            if (aValue > bValue) {
+                return sortDirection === 'asc' ? 1 : -1;
+            }
+            return 0;
+        });
+    };
+
+    const onHeaderClick = (field) => {
+        if (sortField === field) {
+            setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
+        } else {
+            setSortField(field);
+            setSortDirection('asc');
+        }
+    };
+
+    const renderSortIcon = (field) => {
+        return sortField === field ? (sortDirection === 'asc' ? ' ↑' : ' ↓') : '';
+    };
+
+
     function renderButtons(auction) {
         return (auction.seller_name === username ? (
-                <td className='table-data'>
-                    <div className="row">
-                        <button className="btn btn-warning col-6 d-md-flex"
-                                onClick={() => initEditAuction(auction)}>Edit
-                        </button>
-                        <button className="btn btn-danger col-6 d-md-flex"
-                                onClick={() => cancelAuction(auction.id)}>Cancel
-                        </button>
-                    </div>
-                </td>
-            ) : (
-                <td className='table-data'>
-                    <div className="row">
-                        <button className="btn btn-primary col-6 d-md-flex" onClick={() => initChat(auction)}>Chat
-                        </button>
-                        <button className="btn btn-success col-6 d-md-flex" onClick={() => buyAuction(auction.id)}>Buy
-                        </button>
-                    </div>
-                </td>
-            )
+            <td className='table-data'>
+                <div className="row">
+                    <button className="btn btn-warning col-6 d-md-flex"
+                        onClick={() => initEditAuction(auction)}>Edit
+                    </button>
+                    <button className="btn btn-danger col-6 d-md-flex"
+                        onClick={() => cancelAuction(auction.id)}>Cancel
+                    </button>
+                </div>
+            </td>
+        ) : (
+            <td className='table-data'>
+                <div className="row">
+                    <button className="btn btn-primary col-6 d-md-flex" onClick={() => initChat(auction)}>Chat
+                    </button>
+                    <button className="btn btn-success col-6 d-md-flex" onClick={() => buyAuction(auction.id)}>Buy
+                    </button>
+                </div>
+            </td>
+        )
         )
     }
 
@@ -144,6 +182,8 @@ function Auctions({
         setIsEditAuctionActive(!isEditAuctionActive);
     };
 
+    const sortedAuctions = sortAuctions();
+
     return (
         <div>
             <div className="row">
@@ -154,7 +194,7 @@ function Auctions({
                     {isLoggedIn ? (
                         <div>
                             <button className="btn btn-primary col-6 d-md-flex"
-                                    onClick={() => toggleAddAuction()}>Create new
+                                onClick={() => toggleAddAuction()}>Create new
                             </button>
                             <button className="btn btn-primary col-6 d-md-flex" onClick={() => toggleChats()}>Chat with
                                 clients
@@ -164,47 +204,45 @@ function Auctions({
                 </div>
             </div>
             {isAddAuctionActive ? (
-                <AddAuction toggle={toggleAddAuction} onSave={addAuction}/>
+                <AddAuction toggle={toggleAddAuction} onSave={addAuction} />
             ) : null}
             {isEditAuctionActive ? (
-                <EditAuction auction={activeAuction} toggle={toggleEditAuction} onSave={editAuction}/>
+                <EditAuction auction={activeAuction} toggle={toggleEditAuction} onSave={editAuction} />
             ) : null}
             {isChatActive ? (
-                <Chat auction={activeAuction} username={username} toggle={toggleChat}/>
+                <Chat auction={activeAuction} username={username} toggle={toggleChat} />
             ) : null}
             {isChatsActive ? (
-                <Chats auction={activeAuction} username={username} toggle={toggleChats}/>
+                <Chats auction={activeAuction} username={username} toggle={toggleChats} />
             ) : null}
 
             <table className="table table-striped">
                 <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Desc</th>
-                    <th>Status</th>
-                    <th>Price</th>
-                    <th>Seller</th>
-                    {isLoggedIn ? (
-                        <th>Options</th>
-                    ) : null}
-                </tr>
+                    <tr>
+                        <th onClick={() => onHeaderClick('title')}>Name{renderSortIcon('title')}</th>
+                        <th onClick={() => onHeaderClick('body')}>Desc{renderSortIcon('body')}</th>
+                        <th onClick={() => onHeaderClick('status')}>Status{renderSortIcon('status')}</th>
+                        <th onClick={() => onHeaderClick('price')}>Price{renderSortIcon('price')}</th>
+                        <th onClick={() => onHeaderClick('seller_name')}>Seller{renderSortIcon('seller_name')}</th>
+                        {isLoggedIn && <th>Options</th>}
+                    </tr>
                 </thead>
                 <tbody>
-                {
-                    auctions.map((auction, key) =>
+                    {
+                        sortedAuctions.map((auction, key) =>
 
-                        <tr key={key}>
-                            <td className='table-data'>{auction.title}</td>
-                            <td className='table-data'>{auction.body}</td>
-                            <td className='table-data'>{FullStatus[auction.status]}</td>
-                            <td className='table-data'>${auction.price}</td>
-                            <td className='table-data'>{auction.seller_name}</td>
-                            {isLoggedIn ? (
-                                renderButtons(auction)
-                            ) : null}
-                        </tr>
-                    )
-                }
+                            <tr key={key}>
+                                <td className='table-data'>{auction.title}</td>
+                                <td className='table-data'>{auction.body}</td>
+                                <td className='table-data'>{FullStatus[auction.status]}</td>
+                                <td className='table-data'>${auction.price}</td>
+                                <td className='table-data'>{auction.seller_name}</td>
+                                {isLoggedIn ? (
+                                    renderButtons(auction)
+                                ) : null}
+                            </tr>
+                        )
+                    }
                 </tbody>
             </table>
         </div>
