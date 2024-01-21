dad37034ded2a63060a7dbd1837dfc1413b5ca68
Bartosz Jagłowski <b.jaglowski.637@studms.ug.edu.pl>
2024-01-21 10:22:41 +0100

    Refresh after cancel

--- frontend/src/Auctions.jsx
+++ frontend/src/Auctions.jsx
@@ -63,6 +63,7 @@ function Auctions({
             .then((res) => {
                 if (res)
                     alert(res.data.message);
+                refreshAuctions()
             });
     }
 
