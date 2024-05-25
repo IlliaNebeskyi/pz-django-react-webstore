4904bdede1904d91951f37d746195eca1b80658c
Illia Nebeskyi <illianebeskyi@gmail.com>
2024-05-25 06:30:55 +0200

    Add ASGI application instead of WSGI

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'channels',
     'app'
 ]
 
@@ -73,6 +74,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
+ASGI_APPLICATION = 'app.asgi.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
